ae4a9934b5578a0f5b43f7180e0a7903063f1474
Persssekkk <116879454+Persssekkk@users.noreply.github.com>
2022-12-30 22:33:26 +0300

    Update export.py

--- lab24/PythonCode/export.py
+++ lab24/PythonCode/export.py
@@ -1,1 +1,14 @@
+import psycopg2
 
+con = psycopg2.connect("dbname=test user=user1 password=123 host=192.168.1.36 port=5432")
+cur = con.cursor()
+cur.execute("SELECT x, y FROM fn ORDER BY x;")
+arr = cur.fetchall()
+cur.close()
+con.close()
+
+f = open("sin.csv", "w")
+
+for row in arr:
+    f.write(f"{row[0]},{row[1]}\n")
+f.close()
