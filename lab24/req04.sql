d78dd20747cbc4f307fa836eaa353a9896904e97
Persssekkk <116879454+Persssekkk@users.noreply.github.com>
2022-12-30 22:27:25 +0300

    Create req04.sql

---
+++ lab24/req04.sql
@@ -0,0 +1,6 @@
+CREATE TABLE positive_tab(x FLOAT, y FLOAT);
+
+CREATE RULE "_RETURN" AS ON SELECT TO positive_tab DO INSTEAD
+	SELECT * FROM fn WHERE y > 0.0;
+	
+SELECT * FROM positive_tab ORDER BY x LIMIT 15;
